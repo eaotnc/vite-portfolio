ab1b522e14d7b0988b24a1fe9607aaca07277786
Thanatcha Bizcuit <thanatcha@bizcuitsolution.com>
2023-10-10 01:29:16 +0700

    chore: install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.scss";
 import { Button } from "antd";
+import NavBar from "./components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,8 @@ function App() {
   return (
     <>
       <div>
+        <NavBar />
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import "./Navbar.scss";
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="item">Home </div>
+      <div className="item">About Me </div>
+      <div className="item">Skills </div>
+      <div className="item mr-8">Experience </div>
+    </div>
+  );
+};
+
+export default NavBar;
