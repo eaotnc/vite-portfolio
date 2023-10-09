f64d58f82b96b104760d5b1d7be2283712f933fd
thanatcha github <bonsai.eao@gmail.com>
2023-10-10 04:20:52 +0700

    feat: update profile pic

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import profilepic from "../../assets/profile.png";
+import profilepic from "../../assets/profile-big.png";
 import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./Home.scss";
