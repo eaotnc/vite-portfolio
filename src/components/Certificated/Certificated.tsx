6ca188fa4f10adf6ae23f568aaca16ed7ef95f5a
thanatcha github <bonsai.eao@gmail.com>
2023-10-29 04:17:01 +0700

    fix: responsive

--- src/components/Certificated/Certificated.tsx
+++ src/components/Certificated/Certificated.tsx
@@ -7,40 +7,38 @@ import { Reveal } from "../common/Reveal";
 const Certificated = React.forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <div ref={ref} className="certificated">
-      <div className="">
-        <Reveal>
-          <div className="header">Certificate (online)</div>
-        </Reveal>
-        <Reveal>
-          <div className="project">
-            <div className="project-name">
-              Front End Development Libraries (React) - FreeCodeCamp{" "}
-            </div>
-            <img className="cert-image" src={reactcert} alt="" />
-            <a
-              href="https://www.freecodecamp.org/certification/fccdc702980-a234-4eb4-afaa-490bda8906a1/front-end-development-libraries"
-              target="_blank"
-            >
-              https://www.freecodecamp.org/certification/fccdc702980-a234-4eb4-afaa-490bda8906a1/front-end-development-libraries
-            </a>
+      <Reveal>
+        <div className="header">Certificate (online)</div>
+      </Reveal>
+      <Reveal>
+        <div className="project">
+          <div className="project-name">
+            Front End Development Libraries (React) - FreeCodeCamp{" "}
           </div>
-        </Reveal>
-        <Reveal>
-          <div className="project">
-            <div className="project-name">
-              Responsive Web Design - FreeCodeCamp
-            </div>
-            <img className="cert-image" src={html} alt="" />
+          <img className="cert-image" src={reactcert} alt="" />
+          <a
+            href="https://www.freecodecamp.org/certification/fccdc702980-a234-4eb4-afaa-490bda8906a1/front-end-development-libraries"
+            target="_blank"
+          >
+            https://www.freecodecamp.org/certification/fccdc702980-a234-4eb4-afaa-490bda8906a1/front-end-development-libraries
+          </a>
+        </div>
+      </Reveal>
+      <Reveal>
+        <div className="project">
+          <div className="project-name">
+            Responsive Web Design - FreeCodeCamp
+          </div>
+          <img className="cert-image" src={html} alt="" />
 
-            <a
-              href="https://www.freecodecamp.org/certification/fccdc702980-a234-4eb4-afaa-490bda8906a1/responsive-web-design"
-              target="_blank"
-            >
-              https://www.freecodecamp.org/certification/fccdc702980-a234-4eb4-afaa-490bda8906a1/responsive-web-design
-            </a>
-          </div>
-        </Reveal>
-      </div>
+          <a
+            href="https://www.freecodecamp.org/certification/fccdc702980-a234-4eb4-afaa-490bda8906a1/responsive-web-design"
+            target="_blank"
+          >
+            https://www.freecodecamp.org/certification/fccdc702980-a234-4eb4-afaa-490bda8906a1/responsive-web-design
+          </a>
+        </div>
+      </Reveal>
     </div>
   );
 });
