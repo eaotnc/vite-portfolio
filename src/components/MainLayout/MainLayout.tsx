72c02a3152d3a1aaa5fd68ad20c829dfb52fc3fb
thanatcha github <bonsai.eao@gmail.com>
2023-11-02 21:13:37 +0700

    chore: delete animated cursor

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,4 +1,3 @@
-import AnimatedCursor from "react-animated-cursor";
 import { motion, useScroll } from "framer-motion";
 
 interface IMainLayout {
@@ -13,19 +12,6 @@ const MainLayout: React.FC<IMainLayout> = ({ children }) => {
       <motion.div
         className="progress-bar"
         style={{ scaleX: scrollYProgress }}
-      />
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={35}
-        innerScale={1}
-        outerScale={2}
-        outerAlpha={0}
-        innerStyle={{
-          backgroundColor: "#48bdc9",
-        }}
-        outerStyle={{
-          border: "3px solid #48bdc9",
-        }}
       />
       <div>{children}</div>
     </>
